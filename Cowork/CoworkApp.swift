4aac9d65fba8b1a4ae9c78b113884c5d7c83b481
Yaroslav Orlov <ulverforest@gmail.com>
2025-03-04 14:36:56 +0200

    Initial Commit

---
+++ Cowork/CoworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoworkApp.swift
+//  Cowork
+//
+//  Created by Yaroslav Orlov on 04.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
